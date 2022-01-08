aaa6b84f10c44545f9211dce3c49052ac232b851
root <root@ip-172-31-40-46.ap-south-1.compute.internal>
2022-01-08 01:18:48 +0000

    test satge

--- a.sh
+++ a.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo "Today date is :- "
 date
+
+echo "current month cal :-"
 cal
+
+echo "persent working dir :- "
 pwd
+
+echo " current user :- "
 whoami
+
+echo "all files :-"
 ls -a
